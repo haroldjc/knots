14545072e160b51731a98465e60e8cd7882d85af
Harold Coronado <harold@blakzr.com>
2021-07-18 21:50:05 -0500

    Knots CSS structure

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# webpack-babel-sass-boilerplate
-My own webpack, Babel, Sass, Postcss boilerplate
+# Knots CSS
+
+This is a minimal CSS boilerplate/framework I use for my own projects. It uses the latest CSS standards and it targets only modern browsers.
+
+It uses SASS for its compatibility with the BEM methodology.
+
+See the demo here: [https://haroldjc.github.io/knots/](https://haroldjc.github.io/knots/).
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,3 @@
 import * as power from "./utils/functions";
 
-const inputSquare = document.querySelector('.math-form [name="square"]');
-const inputCube = document.querySelector('.math-form [name="cube"]');
-
-function changeValue(event) {
-    if (event.target.value == '')
-        return;
-
-    if (/^\d+$/.test(event.target.value)) {
-        let number = Number(event.target.value);
-        document.querySelector('.roots').innerHTML = `Square power of ${number} is: ${power.square(number)}`;
-    } else {
-        alert('Please, enter only numeric values.');
-    }
-}
-
-inputSquare.addEventListener('keyup', changeValue);
-inputCube.addEventListener('keyup', changeValue);
+console.log(power.cube(9));
